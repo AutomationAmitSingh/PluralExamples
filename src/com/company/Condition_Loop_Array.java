3c211ce4564d000f1ef15ecd8774d5ba7b0f7671
Amit Singh <https://github.com/AutomationAmitSingh/JavaPluralExamples.git>
2019-03-15 23:55:38 +0530

    one more condition loop array class with main some more

--- src/com/company/Condition_Loop_Array.java
+++ src/com/company/Condition_Loop_Array.java
@@ -4,6 +4,10 @@ public class Condition_Loop_Array {
 
     private float students = 0.0f;
     private float rooms = 4.0f;
+    private double val1 = 100.0d;
+    private double val2 = 0.0d;
+    private double result;
+    private char opcode = 'd';
 
     public Condition_Loop_Array()
     {
@@ -19,6 +23,22 @@ public class Condition_Loop_Array {
         }else
             {
                 System.out.println(" No Students");
+            }
+        if(opcode == 'a')
+        {
+          result = val1 + val2;
+        }else if(opcode == 's')
+        {
+        result = val1 - val2;
+        }else if(opcode == 'd')
+        {
+          result = val2 != 0.0d ? val1/val2 : 0.0d;
+        }else if(opcode == 'm')
+        {
+          result = val1 * val2;
+        }else
+            {
+                System.out.println(" Error - Invalid Code");
             }
         System.out.println("** End Program** ");
    }
